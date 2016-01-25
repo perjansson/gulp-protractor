92ee1b6b93de60b9af4a947968cf67b0f44a0f49
Per Jansson <per.r.jansson@gmail.com>
2016-01-25 21:00:40 +0100

    Starting server automatically before protractor

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
-var gulp = require("gulp");
-var protractor = require("gulp-protractor").protractor;
+var gulp = require("gulp"),
+    connect = require('gulp-connect'),
+    protractor = require("gulp-protractor").protractor;
 
-gulp.task('e2e', function() {
+gulp.task('connect', function() {
+  connect.server({
+    port: 8080
+  });
+});
+
+gulp.task('e2e', ['connect'], function() {
   return gulp.src(["test.js"])
     .pipe(protractor({
-      configFile: "protractor.config.js"
+      configFile: "protractor.config.js",
+      args: ['--baseUrl', 'http://127.0.0.1:8080']
     }))
     .on('error', function(e) {
       throw e
--- protractor.config.js
+++ protractor.config.js
@@ -2,8 +2,6 @@ exports.config = {
 
   seleniumServerJar: './resources/selenium/selenium-server-standalone-2.49.1.jar',
 
-  chromeDriver: './resources/chromedriver/chromedriver',
-
-  baseUrl: 'http://127.0.0.1:8080'
+  chromeDriver: './resources/chromedriver/chromedriver'
 
 }
